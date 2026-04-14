4da60be3518765580ba7245daa7a9eb4efd10101
Greyhoods <powerade158@hotmail.com>
2026-04-14 20:32:09 +1200

    Initial Commit

--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -1,18 +1,13 @@
-﻿Console.WriteLine("Hello, C# Academy! One more modification"); Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
-Console.WriteLine("Hello, C# Academy! One more modification");
+﻿void MyFirstMethod()
+{
+    //Do Something
+}
 
+void MySecondMethod()
+{
+    //Do Something
+}
+void MyThirdMethod()
+{
+    //Do Something
+}
